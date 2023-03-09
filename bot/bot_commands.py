69e3989becd60691c13eacfd19b288388e08648f
EvgeniyNikNik <nievni@gmail.com>
2023-03-09 22:14:22 +0300

    mess handler

--- bot/bot_commands.py
+++ bot/bot_commands.py
@@ -78,10 +78,10 @@ async def game(call: types.CallbackQuery):
         video_text = "38.8"
         image = types.InputFile("./images/sale_images/388.png")
         text = results_text["38.8"]
-    #await call.message.answer_video(video=video)
+    await call.message.answer(video=open("./videos/388.mp4", "rb"))
     await asyncio.sleep(15)
     await call.message.answer_photo(photo=image, caption=text)
 
 @dp.message_handler()
 async def aaaa(message: types.Message):
-    print(message.video.file_id)
+    print(message)
