550dd1e87e92ac005e99417c0370e13da2a94d5c
EvgeniyNikNik <nievni@gmail.com>
2023-03-09 22:36:47 +0300

    mess handler

--- bot/bot_commands.py
+++ bot/bot_commands.py
@@ -80,7 +80,6 @@ async def game(call: types.CallbackQuery):
         image = types.InputFile("./images/sale_images/388.png")
         text = results_text["38.8"]
     #await call.message.answer_document(open("./videos/388.mp4", "rb"))
-    await aiogram.methods.send_video.SendVideo(call.from_user.id, open("./videos/388.mp4", "rb"))
     await asyncio.sleep(15)
     await call.message.answer_photo(photo=image, caption=text)
 
